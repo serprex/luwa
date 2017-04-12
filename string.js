4298dc762fc47e50213c290819bf0a1559d1727a
Demur Rumed <gunkmute@gmail.com>
2017-04-12 10:37:55 +0000

    Remove vm param from string/utf8, string.reverse

--- string.js
+++ string.js
@@ -4,23 +4,33 @@ var string = module.exports = new Table();
 string.set("char", utf8.get("char"));
 string.set("len", string_len);
 string.set("lower", string_lower);
+string.set("reverse", string_reverse);
 string.set("upper", string_upper);
 
-function*string_len(vm, stack, base) {
+function*string_len(stack, base) {
 	let s = util.readarg(stack, base+1);
 	if (typeof s != "string") throw "string.lower #1: expected string";
 	stack[base] = s.length;
 	stack.length = base + 1;
 }
 
-function*string_lower(vm, stack, base) {
+function*string_lower(stack, base) {
 	let s = util.readarg(stack, base+1);
 	if (typeof s != "string") throw "string.lower #1: expected string";
 	stack[base] = s.toLowerCase();
 	stack.length = base + 1;
 }
 
-function*string_upper(vm, stack, base) {
+function*string_reverse(stack, base) {
+	let s = util.readarg(stack, base+1);
+	if (typeof s != "string") throw "string.reverse #1: expected string";
+	let r = '';
+	for (let i=s.length-1; i>=0; i--) r += s[i];
+	stack[base] = r;
+	stack.length = base + 1;
+}
+
+function*string_upper(stack, base) {
 	let s = util.readarg(stack, base+1);
 	if (typeof s != "string") throw "string.upper #1: expected string";
 	stack[base] = s.toUpperCase();
