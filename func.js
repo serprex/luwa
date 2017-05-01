1236e858216f983340399fa47fd9387291df427f
Demur Rumed <gunkmute@gmail.com>
2017-05-01 00:14:30 +0000

    Store bc as Uint32Array (would prefer Uint8 but that'll require reworking offsets)

--- func.js
+++ func.js
@@ -6,7 +6,7 @@ function Func(asm) {
 	this.id = asm.id;
 	this.sn = asm.lx.snr;
 	this.ss = asm.lx.ssr;
-	this.bc = asm.bc;
+	this.bc = new Uint32Array(asm.bc);
 	this.fus = asm.fus;
 	this.pcount = asm.pcount;
 	this.lcount = asm.lcount;
@@ -16,7 +16,7 @@ function Func(asm) {
 	this.local2free = asm.local2free;
 	this.trace = new trace.Context();
 	this.labels = new Set();
-	for (var i=0; i<asm.gotos.length; i+=3) {
+	for (let i=0; i<asm.gotos.length; i+=3) {
 		let lpos = asm.labelpos[asm.gotos[i+1]];
 		this.bc[asm.gotos[i]] = lpos.pos;
 		this.labels.add(lpos.pos);
