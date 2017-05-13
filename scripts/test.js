a6fe261bdbfc07761ec7fc0bb4abae693e1b92d9
Demur Rumed <gunkmute@gmail.com>
2017-05-13 01:55:51 +0000

    Fix test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 const assert = require("assert"),
-	lua = require("./luwa"),
-	e = require("./env")();
+	lua = require("../luwa"),
+	e = require("../env")();
 
 lua.eval("x = 3 * 5 + 2", e);
 assert.equal(e.get("x"), 17);
