84d3cd2461e1bc64b014448418858074d322a879
Demur Rumed <gunkmute@gmail.com>
2017-05-22 02:56:40 +0000

    Pivoting to full blown GC, still need to write the GC part

--- rt.js
+++ rt.js
@@ -1,4 +1,9 @@
-const rtwa = fetch('rt.wasm').then(r => r.arrayBuffer());
+const rtwa = typeof fetch !== 'undefined' ?
+	fetch('rt.wasm').then(r => r.arrayBuffer()) :
+	new Promise((resolve, reject) =>
+		require('fs').readFile(__dirname + '/rt.wasm',
+			(err, data) => err ? reject(err) : resolve(data.buffer.slice(data.byteOffset, data.byteOffset + data.byteLength)))
+	);
 module.exports = imp => rtwa.then(
 	ab => {
 		const mem = new WebAssembly.Memory({initial:1});
