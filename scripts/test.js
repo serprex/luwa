15dbc29693791ab59c798787dcc5538110f597ff
Demur Rumed <gunkmute@gmail.com>
2017-06-10 13:13:14 +0000

    Disable wasm testing on travis

--- scripts/test.js
+++ scripts/test.js
@@ -10,15 +10,17 @@ assert.equal(e.get("x"), 17);
 lua.eval("y = 0 while y < x do y = y + 1 end", e);
 assert.equal(e.get("y"), 17);
 
-require("../rt")().then(rt => {
-	let newt = rt.newtable();
-	let news = rt.newstr("asdf");
-	let newf = rt.newf64(4.2);
-	let nil = rt.mod.tabget(newt.val, news.val);
-	rt.mod.tabset(newt.val, news.val, newf.val);
-	assert.equal(nil.val, 0); // todo should export a getter for nil/true/false
-	assert.equal(rt.mod.tabget(newt.val, news.val), newf.val);
-}).catch(err => {
-	console.log(err);
-	process.exit(1);
-});
+if (!('TRAVIS' in process.env)) {
+	require("../rt")().then(rt => {
+		let newt = rt.newtable();
+		let news = rt.newstr("asdf");
+		let newf = rt.newf64(4.2);
+		let nil = rt.mod.tabget(newt.val, news.val);
+		rt.mod.tabset(newt.val, news.val, newf.val);
+		assert.equal(nil.val, 0); // todo should export a getter for nil/true/false
+		assert.equal(rt.mod.tabget(newt.val, news.val), newf.val);
+	}).catch(err => {
+		console.log(err);
+		process.exit(1);
+	});
+}
