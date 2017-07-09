adb8ee41aec6a21c5f1105015f4315623b16aeae
Demur Rumed <gunkmute@gmail.com>
2017-07-09 17:09:43 +0000

    table.insert table.remove table.move

--- prelude.lua
+++ prelude.lua
@@ -15,6 +15,47 @@ function table.pack(...)
 	return { n = select('#', ...), ... }
 end
 
+function table.insert(list, pos, value)
+	if not value then
+		list[#list+1] = pos
+	else
+		for i = #list,pos,-1 do
+			list[i+1] = list[i]
+		end
+		list[pos] = value
+	end
+end
+
+function table.remove(list, pos)
+	local ret
+	if pos then
+		ret = list[pos]
+		for i = pos,#list do
+			list[i] = list[i+1]
+		end
+	else
+		ret = list[#list]
+		list[#list] = nil
+	end
+	return ret
+end
+
+function table.move(a1, f, e, t, a2)
+	if not a2 then
+		a2 = a1
+	end
+	local len1 = e - f - 1
+	if t < f then
+		for i=0,len do
+			a[t+i] = a[f+i]
+		end
+	elseif t > f then
+		for i=len,0,-1 do
+			a[t+i] = a[f+i]
+		end
+	end
+end
+
 function assert(v, message, ...)
 	if v then
 		return error(message, 2)
