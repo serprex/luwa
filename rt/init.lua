2b87d7ee29fb21dbe53b0141ff748bf4a05800e1
Demur Rumed <gunkmute@gmail.com>
2017-07-12 12:54:59 +0000

    Move otmpstack init to data section too

--- rt/init.lua
+++ rt/init.lua
@@ -3,9 +3,9 @@ TRUE = 8
 FALSE = 16
 HEAPBASE = 24
 otmp = global(i32, true)
-otmpstack = global(i32, true)
+otmpstack = global(i32, true, HEAPBASE)
 otmpstacklen = global(i32, true)
-heaptip = global(i32, true, HEAPBASE)
+heaptip = global(i32, true, HEAPBASE + 48) -- 48 == allocsize(vec.base + 32)
 markbit = global(i32, true)
 
 data(0, 0, {
@@ -15,10 +15,6 @@ data(0, 0, {
 	0, 0, 0, 0, 3, 0, 0 ,0,
 	-- true
 	0, 0, 0, 0, 3, 1, 0, 0,
+	-- otmpstack = vec(32)
+	0, 0, 0, 0, 6, 32, -- 35 zeroes
 })
-
-init = start(func(function(f)
-	f:i32(32)
-	f:call(newvec)
-	f:storeg(otmpstack)
-end))
