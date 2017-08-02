a269410967a89a523fd09decb9dcdd12a0977a36
Demur Rumed <gunkmute@gmail.com>
2017-08-02 02:55:40 +0000

    Sketch RETURN & pcall
    Move std.lua to util.lua, std.lua will now be used for stdlib code

--- rt/vm.lua
+++ rt/vm.lua
@@ -1,16 +1,27 @@
+--[[
+VM needs to support both coroutines & yielding to JS thread at frequent intervals
+
+stack frame consists of
+tmpstack: locals, frees, consts, bc, stack
+datastack: callty, pc, retc, base, pc
+
+callty is
+0 norm Reload locals
+1 init Return stack
+2 prot Reload locals
+3 call Continue call chain
+
+pcall sets up stack frame & returns control to calling VM loop. No nested VM loops
+]]
+
 eval = func(i32, i32, function(f)
-	-- stack frame consists of
-	-- tmpstack: locals, frees, consts, bc, stack
-	-- datastack: base, ispcall?, pc
 	local a, b = f:locals(i32, 2)
 	local locals, frees, consts, bc, stack, ispcall, pc = f:locals(i32, 8)
 
 	f:loadg(odatastack)
 	f:loadg(odatastacklen)
 	f:add()
-	f:i32(4)
-	f:sub()
-	f:i32load()
+	f:i32load(str.base - 4)
 	f:store(pc)
 
 	f:i32(4)
@@ -91,7 +102,82 @@ eval = func(i32, i32, function(f)
 		f:br(nop)
 	end) -- RETURN
 	-- pop stack frame
-		f:br(nop)
+
+		f:loadg(odatastack)
+		f:loadg(odatastacklen)
+		f:add()
+		assert(str.base >= 12)
+		f:i32load(str.base - 12)
+		f:store(a)
+
+		f:call(tmppop)
+		f:call(tmppop)
+		f:call(tmppop)
+		f:call(tmppop)
+		f:call(tmppop)
+		f:i32(17)
+		f:call(dealloca)
+
+		f:block(function(endprog)
+			f:block(function(loadframe)
+				-- read callty from freed memory
+				f:loadg(odatastack)
+				f:loadg(odatastacklen)
+				f:add()
+				f:i32load8(str.base)
+				f:brtable(loadframe, endprog, loadframe)
+			end) -- loadframe
+			-- TODO -1 should always have a special case
+			-- Address once I've worked out call chains
+			f:load(a)
+			f:i32(-1)
+			f:ne()
+			f:load(stack)
+			f:i32load(buf.len)
+			f:tee(b)
+			f:load(a)
+			f:ne()
+			f:band()
+			-- check ne to avoid lt/gt checks most of the time
+			f:iff(function()
+				f:load(b)
+				f:load(a)
+				f:gtu()
+				f:iff(function()
+					-- shrink stack
+					f:loop(function(loop)
+						f:load(stack)
+						f:call(popvec)
+						f:load(b)
+						f:i32(1)
+						f:sub()
+						f:tee(b)
+						f:load(a)
+						f:gtu()
+						f:brif(loop)
+					end)
+				end, function()
+					-- pad stack with nils
+					f:loop(function(loop)
+						f:load(stack)
+						f:i32(NIL)
+						f:call(pushvec)
+						f:store(stack)
+						f:load(b)
+						f:i32(1)
+						f:add()
+						f:tee(b)
+						f:load(a)
+						f:ltu()
+						f:brif(loop)
+					end)
+				end)
+			end)
+			-- LOADFRAME
+			f:br(nop)
+		end) -- endprog
+		f:load(stack)
+		f:ret()
 	end) -- CALL
 	-- push stack frame header
 		f:br(nop)
