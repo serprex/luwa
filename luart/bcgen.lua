9df4794828b4f8503cdc19392426fa5e56a7d477
Demur Rumed <gunkmute@gmail.com>
2017-10-15 22:49:03 +0000

    Sketching out a better datastructure for scoping than what bc.js uses

--- luart/bcgen.lua
+++ luart/bcgen.lua
@@ -10,7 +10,8 @@ local function Assembler(lx, pcount, isdotdotdot, uplink)
 		pcount = pcount,
 		isdotdotdot = isdotdotdot,
 		uplink = uplink,
-		scopes = {},
+		scopedata = {},
+		names = {},
 		bc = {},
 	}, asmmt)
 end
@@ -21,10 +22,30 @@ function asmmeta:push(op, ...)
 		self.bc[#self.bc+1],self.bc[#self.bc+2],self.bc[#self.bc+3],self.bc[#self.bc+4] = string.pack('<i4', select(i, ...))
 	end
 end
-function asmmeta:scope()
+function asmmeta:scope(f)
+	local nsdata = #self.scopedata
+	f(self)
+	for i=#self.scopedata, nsdata, -1 do
+		local sdata = self.scopedata
+		local sname = sdata.name
+		self.names[sname] = self.names[sname].prev
+	end
 end
 
-function asmmeta:name()
+function asmmeta:name(n, isparam)
+	local prevscope = self.names[n]
+	local newscope = { prev = prevscope, isparam = isparam }
+	self.scopedata[#self.scopedata+1] = newscope
+	self.names[n] = newscope
+end
+
+function asmmeta:usename(n)
+	local name = self.names[n]
+	if name then
+		name.isfree = true
+	else
+		self.names[1].isfree = true -- _ENV
+	end
 end
 
 function asmmeta:scopeBlock(node)
@@ -39,7 +60,7 @@ end
 return function(lx, root)
 	local asm = Assembler(lx, tree, true, nil)
 	asm.scope(function()
-		asm.name(0, -1) -- _ENV
+		asm.name(1, true) -- _ENV
 		asm.scopeBlock(root)
 	end)
 	asm.genBlock(root)
