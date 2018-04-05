6f7d293c6f5bbf582ed722cccc9a2a08eb47e72b
Demur Rumed <gunkmute@gmail.com>
2018-04-05 04:52:57 +0000

    Replace seq with using multiplication. Parsing is failing because many isn't backtracking for LPeg. Need to work that out

--- rt/astgen3.lua
+++ rt/astgen3.lua
@@ -21,14 +21,8 @@ end
 local function maybe(rule)
 	return rule^-1
 end
-local function seq(x, ...)
-	for i=1,select('#',...) do
-		x = x*select(i, ...)
-	end
-	return x
-end
-local function sf(id, ...)
-	return seq(...) / function(...) return { tag = id, ... } end
+local function sf(id, rule)
+	return rule / function(...) return { tag = id, ... } end
 end
 local function of(id, x, ...)
 	for i=1,select('#',...) do
@@ -38,43 +32,43 @@ local function of(id, x, ...)
 end
 local Block, Stat, Var, ExpOr, ExpAnd, Exp, Prefix, Args, Funcbody, Table, Field, Binop, Unop, Value, Index, Suffix =
 	V"Block", V"Stat", V"Var", V"ExpOr", V"ExpAnd", V"Exp", V"Prefix", V"Args", V"Funcbody", V"Table", V"Field", V"Binop", V"Unop", V"Value", V"Index", V"Suffix"
-local Explist = seq(ExpOr, many(seq(slex._comma, ExpOr)))
-local Namelist = seq(name, many(seq(slex._comma, name)))
-local Varlist = seq(Var, many(seq(slex._comma, Var)))
+local Explist = ExpOr * many(slex._comma * ExpOr)
+local Namelist = name * many(slex._comma * name)
+local Varlist = Var * many(slex._comma * Var)
 local Fieldsep = slex._comma + slex._semi
-local Call = seq(maybe(seq(slex._colon, name)), Args)
-local Funccall = seq(Prefix, many(Suffix), Call)
+local Call = maybe(slex._colon * name) * Args
+local Funccall = Prefix * many(Suffix) * Call
 local Grammar = P {
 	Block * P("\0");
-	Block = sf(ast.Block, many(Stat), maybe(seq(slex._return, maybe(Explist), maybe(slex._semi)))),
+	Block = sf(ast.Block, many(Stat) * maybe(slex._return * maybe(Explist) * maybe(slex._semi))),
 	Stat = of(ast.Stat,
 		slex._semi,
-		seq(Varlist, slex._set, Explist),
+		Varlist * slex._set * Explist,
 		Funccall,
-		seq(slex._label, name, slex._label),
+		slex._label * name * slex._label,
 		slex._break,
-		seq(slex._goto, name),
-		seq(slex._do, Block, slex._end),
-		seq(slex._while, ExpOr, slex._do, Block, slex._end),
-		seq(slex._repeat, Block, slex._until, ExpOr),
-		seq(slex._if, ExpOr, slex._then, Block, many(seq(slex._elseif, ExpOr, slex._then, Block)), maybe(seq(slex._else, Block)), slex._end),
-		seq(slex._for, name, slex._set, ExpOr, slex._comma, ExpOr, maybe(seq(slex._comma, ExpOr)), slex._do, Block, slex._end),
-		seq(slex._for, Namelist, slex._in, Explist, slex._do, Block, slex._end),
-		seq(slex._function, name, many(seq(slex._dot, name)), maybe(seq(slex._colon, name)), Funcbody),
-		seq(slex._local, slex._function, name, Funcbody),
-		seq(slex._local, Namelist, maybe(seq(slex._set, Explist)))
+		slex._goto * name,
+		slex._do * Block * slex._end,
+		slex._while * ExpOr * slex._do * Block * slex._end,
+		slex._repeat * Block * slex._until * ExpOr,
+		slex._if * ExpOr * slex._then * Block * many(slex._elseif * ExpOr * slex._then * Block) * maybe(slex._else * Block) * slex._end,
+		slex._for * name * slex._set * ExpOr * slex._comma * ExpOr * maybe(slex._comma * ExpOr) * slex._do * Block * slex._end,
+		slex._for * Namelist * slex._in * Explist * slex._do * Block * slex._end,
+		slex._function * name * many(slex._dot * name) * maybe(slex._colon * name) * Funcbody,
+		slex._local * slex._function * name * Funcbody,
+		slex._local * Namelist * maybe(slex._set * Explist)
 	),
-	Var = of(ast.Var, name, seq(Prefix, many(Suffix), Index)),
-	ExpOr = sf(ast.ExpOr, ExpAnd, many(seq(slex._or, ExpAnd))),
-	ExpAnd = sf(ast.ExpAnd, Exp, many(seq(slex._and, Exp))),
-	Exp = of(ast.Exp, seq(Unop, Exp), seq(Value, maybe(seq(Binop, Exp)))),
+	Var = of(ast.Var, name, Prefix * many(Suffix) * Index),
+	ExpOr = sf(ast.ExpOr, ExpAnd * many(slex._or * ExpAnd)),
+	ExpAnd = sf(ast.ExpAnd, Exp * many(slex._and * Exp)),
+	Exp = of(ast.Exp, Unop * Exp, Value * maybe(Binop * Exp)),
 	Prefix = of(ast.Prefix, name, slex._pl * ExpOr * slex._pr),
 	Args = of(ast.Args,
-		seq(slex._pl, maybe(Explist), slex._pr),
+		slex._pl * maybe(Explist) * slex._pr,
 		Table,
 		slit),
-	Funcbody = sf(ast.Funcbody, slex._pl, maybe(seq(Namelist, maybe(seq(slex._comma, slex._dotdotdot))) + slex._dotdotdot), slex._pr, Block, slex._end),
-	Table = sf(ast.Table, slex._cl, maybe(seq(Field, many(seq(Fieldsep, Field)), maybe(Fieldsep))), slex._cr),
+	Funcbody = sf(ast.Funcbody, slex._pl * maybe(Namelist * maybe(slex._comma * slex._dotdotdot) + slex._dotdotdot) * slex._pr * Block * slex._end),
+	Table = sf(ast.Table, slex._cl * maybe(Field * many(Fieldsep * Field) * maybe(Fieldsep)) * slex._cr),
 	Field = of(ast.Field,
 		slex._sl * ExpOr * slex._sr * slex._set * ExpOr,
 		name * slex._set * ExpOr,
