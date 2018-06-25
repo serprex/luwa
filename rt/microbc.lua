9723e62db59d936167be85068721f8d7a7e3c2eb
Demur Rumed <gunkmute@gmail.com>
2018-06-25 01:38:17 +0000

    Pivoting how to specify micro ops
    If we're SSA then we can store the results of calculations to variables. If we use builders like I do for WASM then everything doesn't have to get shoved into expressions. Which means being able to declare variables & no longer having to pass strings all over the place
    
    Haven't gotten into guts of how to organize microbc's assembler. Idea is that all these methods will return a slot reference, where essentially most micro ops take outputs of previous ops in the spec as input. Little messy if I end up with a multiproducing microop
    
    Lost the Int/Ptr/Obj annotations. Ideally that can be tracked by giving opcodes type signatures. Would like to get push/pop in there but while all instructions have constant stack results it is dependent upon instruction arguments at times, so some complications to sort out

--- rt/microbc.lua
+++ rt/microbc.lua
@@ -2,172 +2,251 @@ local bc = require 'bc'
 local alloc = require 'alloc'
 local types = alloc.types
 local mops = {}
+local mopmt__index = {}
+local mopmt = { __index = mopmt__index }
 local function mkMop(op)
 	local id = #mops+1
 	mops[id] = op
 	mops[op] = id
-	return function(...)
-		return { op = op, id = id, ... }
+	mopmt__index[op] = function(f, ...)
+		f.ops[#f.ops] = { op = id, ... }
 	end
 end
-local Nop = mkMop('Nop')
-local Int = mkMop('Int')
-local Load = mkMop('Load')
-local Store = mkMop('Store')
-local StoreName = mkMop('StoreName')
-local StoreNameInt = mkMop('StoreNameInt')
-local LoadName = mkMop('LoadName')
-local Eq = mkMop('Eq')
-local If = mkMop('If')
-local In = mkMop('In')
-local Arg = mkMop('Arg')
-local Push = mkMop('Push')
-local Truthy = mkMop('Truthy')
-local CloneFunc = mkMop('CloneFunc')
-local ObjMetalessEq = mkMop('ObjMetalessEq')
-local IntObjFromInt = mkMop('IntObjFromInt')
-local LoadStrLen = mkOp('LoadStrLen')
-local Error = mkOp('Error')
-local NopAtom = Nop()
-local NilAtom = Int(0)
-local FalseAtom = Int(4)
-local TrueAtom = Int(8)
+local function mkOp(op, func)
+	local f = setmetatable({}, mopmt)
+	func(f)
+	ops[op] = f
+end
+mkMop('Nop')
+mkMop('Int')
+mkMop('Load')
+mkMop('Store')
+mkMop('Eq')
+mkMop('If')
+mkMop('In')
+mkMop('Arg')
+mkMop('Push')
+mkMop('Truthy')
+mkMop('CloneFunc')
+mkMop('ObjMetalessEq')
+mkMop('IntObjFromInt')
+mkMop('LoadStrLen')
+mkMop('Error')
+mkMop('Macro')
+function mopmt__index:Nil()
+	return self:Int(0)
+end
+function mopmt__index:False()
+	return self:Int(4)
+end
+function mopmt__index:True()
+	return self:Int(8)
+end
 local ops = {}
-ops[bc.Nop] = NopAtom
-ops[bc.LoadNil] = Push(NilAtom)
-ops[bc.LoadFalse] = Push(FalseAtom)
-ops[bc.LoadTrue] = Push(TrueAtom)
-ops[bc.LoadParam] = Push(Load(Param(Arg(0))))
-ops[bc.StoreParam] = Store(Param(Arg(0)), Pop())
-ops[bc.LoadLocal] = Push(Load(Local(Arg(0))))
-ops[bc.StoreLocal] = Store(Local(Arg(0)), Pop())
-ops[bc.LoadFree] = Push(Load(Free(Arg(0))))
-ops[bc.LoadFreeBox] = Push(Load(Load(Free(Arg(0)))))
-ops[bc.StoreFreeBox] = Store(Load(Free(Arg(0))), Pop())
-ops[bc.LoadParamBox] = Push(Load(Load(Param(Arg(0)))))
-ops[bc.StoreParamBox] = Store(Load(Param(Arg(0))), Pop())
-ops[bc.BoxParam] = Store(Param(0), Box(Param(Arg(0))))
-ops[bc.BoxLocal] = Store(Local(0), Box(NilAtom))
-ops[bc.LoadLocalBox] = Push(Load(Load(Local(Arg(0)))))
-ops[bc.StoreLocalBox] = Store(Load(Local(Arg(0))), Pop())
-ops[bc.LoadConst] = Push(Load(Const(Arg(0))))
-ops[bc.Pop] = Pop()
-ops[bc.Syscall] = Syscall(Arg(0))
-ops[bc.Jmp] = SetPc(Arg(0))
-ops[bc.JifNot] = If(
-	Truthy(Pop()),
-	SetPc(Arg(0)),
-	NopAtom
-)
-ops[bc.Jif] = If(
-	Truthy(Pop()),
-	NopAtom
-	SetPc(Arg(0))
-)
-ops[bc.LoadFunc] = Seq(
-	StoreName('func', CloneFunc(Const(Arg(1)))),
-	If(
-		Arg(0),
-		Store(Add(LoadName('func'), functy.frees), FillFromStack(NewVec(Arg(0)), Arg(0))),
-		NopAtom
-	),
-	Push(LoadName('func'))
-)
-ops[bc.LoadVarg] = Seq(
-	StoreNamePtr('tmp', AllocateTemp(Arg(0))),
-	StoreNameInt('i', 0),
-	StoreNameInt('vlen', VargLen()),
-	StoreNamePtr('vptr', VargPtr()),
-	If(
-		Lt(LoadNameInt('vlen'), Arg(0)),
-		Seq(
-			StoreNameInt('vlen4', Mul(LoadNameInt('vlen'), Int(2))),
-			MemCpy4(LoadNamePtr('tmp'), LoadNamePtr('vptr'), LoadNameInt('vlen4')),
-			FillRange(Add(LoadNamePtr('tmp'), LoadNameInt('vlen4')), NilAtom, Mul(Sub(Arg(0), LoadNameInt('vlen')), Int(2)))
-		),
-		MemCpy4(LoadNamePtr('tmp'), LoadNamePtr('vptr'), Mul(Arg(0), Int(2)))
+mkOp(bc.Nop, function()
+end)
+mkOp(bc.LoadNil, function(f)
+	f:Push(f:Nil())
+end)
+mkOp(bc.LoadFalse, function(f)
+	f:Push(f:False())
+end)
+mkOp(bc.LoadTrue, function(f)
+	f:Push(f:True())
+end)
+mkOp(bc.LoadParam, function(f)
+	f:Push(f:Load(f:Param(f:Arg(0))))
+end)
+mkOp(bc.StoreParam, function(f)
+	f:Store(f:Param(f:Arg(0)), f:Pop())
+end)
+mkOp(bc.LoadLocal, function(f)
+	f:Push(f:Load(f:Local(f:Arg(0))))
+end)
+mkOp(bc.StoreLocal, function(f)
+	f:Store(f:Local(f:Arg(0)), f:Pop())
+end)
+mkOp(bc.LoadFree, function(f)
+	f:Push(f:Load(f:Free(f:Arg(0))))
+end)
+mkOp(bc.LoadFreeBox, function(f)
+	f:Push(f:Load(f:Load(f:Free(f:Arg(0)))))
+end)
+mkOp(bc.StoreFreeBox, function(f)
+	f:Store(Load(Free(Arg(0))), Pop())
+end)
+mkOp(bc.LoadParamBox, function(f)
+	f:Push(f:Load(f:Load(f:Param(f:Arg(0)))))
+end)
+mkOp(bc.StoreParamBox, function(f)
+	f:Store(f:Load(f:Param(f:Arg(0))), f:Pop())
+end)
+mkOp(bc.BoxParam, function(f)
+	f:Store(f:Param(0), f:Box(f:Param(f:Arg(0))))
+end)
+mkOp(bc.BoxLocal, function(f)
+	f:Store(f:Local(0), f:Box(f:Nil()))
+end)
+mkOp(bc.LoadLocalBox, function(f)
+	f:Push(f:Load(f:Load(f:Local(f:Arg(0)))))
+end)
+mkOp(bc.StoreLocalBox, function(f)
+	f:Store(f:Load(f:Local(f:Arg(0))), f:Pop())
+end)
+mkOp(bc.LoadConst, function(f)
+	f:Push(f:Load(f:Const(f:Arg(0))))
+end)
+mkOp(bc.Pop, function(f)
+	f:Pop()
+end)
+mkOp(bc.Syscall, function(f)
+	f:Syscall(f:Arg(0))
+end)
+mkOp(bc.Jmp, function(f)
+	f:SetPc(f:Arg(0))
+end)
+mkOp(bc.JifNot, function(f)
+	f:If(
+		f:Truthy(f:Pop()),
+		function()
+			f:SetPc(f:Arg(0))
+		end
 	)
-)
-
-ops[bc.AppendVarg] = AppendRange(Pop(), VargPtr(), Arg(0))
+end)
+mkOp(bc.Jif, function(f)
+	f:If(
+		f:Truthy(f:Pop()),
+		function() end,
+		f:SetPc(f:Arg(0))
+	)
+end)
+mkOp(bc.LoadFunc, function(f)
+	local func = f:CloneFunc(f:Const(f:Arg(1)))
+	f:If(
+		f:Arg(0),
+		function()
+			f:Store(
+				f:Add(func, f:Int(functy.frees)),
+				f:FillFromStack(f:NewVec(f:Arg(0)), f:Arg(0))
+			)
+		end
+	)
+	f:Push(func)
+end)
+mkOp(bc.LoadVarg, function(f)
+	local tmp = AllocateTemp(Arg(0))
+	local vlen = VargLen()
+	local vptr = VargPtr()
+	f:If(
+		f:Lt(vlen, f:Arg(0)),
+		function()
+			local vlen4 = f:Mul(vlen, f:Int(2))
+			f:MemCpy4(tmp, vptr, vlen4)
+			f:FillRange(f:Add(tmp, vlen4), NilAtom, f:Mul(f:Sub(f:Arg(0), vlen), f:Int(2)))
+		end,
+		function()
+			f:MemCpy4(tmp, vptr, f:Mul(f:Arg(0), f:Int(2)))
+		end
+	)
+end)
+mkOp(bc.AppendVarg, function(f)
+	f:AppendRange(f:Pop(), f:VargPtr(), f:Arg(0))
+end)
 -- TODO
-ops[bc.Call] = Seq(
-	StoreNameInt('nret', Arg(0)),
-	StoreNameInt('baseframe', DataFrameTop()),
-	StoreNameInt('rollingbase', DataFrameTopBase()),
-	AllocateDataFrames(Arg(1)),
+mkOp(bc.Call, function(f)
+	local nret = f:Arg(0)
+	local baseframe = f:DataFrameTop()
+	local rollingbase = f:DataFrameTopBase()
+	f:AllocateDataFrames(f:Arg(1))
 	-- TODO StoreName 'func'
-	ForRange('i', Arg(1),
-		StoreNameInt(Add('rollingbase', Mul(Arg(LoadNameInt('i')), Int(4)))),
+	ForRange(Int(0), Arg(1), function(i)
+		-- TODO SSA this:
+		-- rollingbase = Add(rollingbase, Mul(Arg(LoadNameInt('i')), Int(4)))
 		WriteDataFrame(
-			Add(LoadNameInt('baseframe'), LoadNameInt('i')),
-			If(LoadNameInt('i'), 3, 1), -- type = i ? call : norm
-			Int(0), -- pc
-			LoadNameInt('rollingbase'), -- base
-			Mul(LoadFuncParamc(LoadName('func')), 4), -- dotdotdot
-			Int(-4), -- retb
-			Int(-1), -- retc
+			f:Add(baseframe, i),
+			f:If(i,
+				function()
+					f:Int(3)
+				end,
+				function()
+					f:Int(1)
+				end), -- type = i ? call : norm
+			f:Int(0), -- pc
+			rollingbase, -- base
+			f:Mul(f:LoadFuncParamc(func), f:Int(4)), -- dotdotdot
+			f:Int(-4), -- retb
+			f:Int(-1), -- retc
 			0, --  TODO calc locals
 			0 -- TODO calc frame
 		)
-	),
-	PushObjFrameFromFunc(LoadName('func')),
-	SetPc(Int(0))
-)
+	end)
+	f:PushObjFrameFromFunc(func)
+	f:SetPc(f:Int(0))
+end)
 
-ops[bc.Not] = Push(
-	If(Truthy(Pop()),
-		FalseAtom,
-		TrueAtom)
-)
+mkOp(bc.Not, function(f)
+	f:Push(
+		f:If(f:Truthy(f:Pop()),
+			function() f:False() end,
+			function() f:True() end)
+	)
+end)
 
-ops[bc.Len] = Seq(
-	StoreName('a', Pop()),
-	StoreNameInt('aty', Type(LoadName('a'))),
-	If(Eq(LoadNameInt('aty'), Int(types.str)),
-		Push(IntObjFromInt(LoadStrLen(LoadName('a')))),
-		If(Eq(LoadNameInt('aty'), Int(types.tbl)),
-			Seq(
-				StoreName('ameta', Meta(LoadName('a'))),
-				If(LoadName('ameta'),
-					CallMetaMethod('__len'), -- TODO helper function this
-					NopAtom)
+mkOp(bc.Len, function(f)
+	local a = f:Pop()
+	local aty = f:Type(a)
+	f:If(
+		f:Eq(aty, f:Int(types.str)),
+		function() f:Push(f:IntObjFromInt(f:LoadStrLen(a))) end,
+		function()
+			f:If(
+				f:Eq(aty, f:Int(types.tbl)),
+				function()
+					local ameta = Meta(a)
+					f:If(ameta,
+						function() f:CallMetaMethod('__len') end -- TODO helper function this
+					)
+				end,
+				function() f:Error() end
+			)
+		end
+	)
+end)
+
+mkOp(bc.TblNew, function(f)
+	f:Push(f:NewTbl())
+end)
+mkOp(bc.TblAdd, function(f)
+	local v = f:Pop()
+	local k = f:Pop()
+	local tbl = f:Pop()
+	f:TblSet(tbl, k, v)
+	f:Pop()
+	f:Pop()
+end)
+
+mkOp(bc.CmpEq, function(f)
+	local a = f:Pop()
+	local b = f:Pop()
+	f:If(
+		f:ObjMetalessEq(a, b),
+		function() f:Push(f:True()) end,
+		f:If(
+			f:And(
+				f:Eq(f:Type(a), f:Int(types.tbl)),
+				f:Eq(f:Type(b), f:Int(types.tbl))
 			),
-			Error()
+			function()
+				f:If(
+					f:Eq(f:Meta(a), f:Meta(b)),
+					function() f:Push(f:True()) end, -- CALL META
+					function() f:Push(f:False()) end
+				)
+			end,
+			function() f:Push(f:False()) end
 		)
 	)
-)
-
-ops[bc.TblNew] = Push(NewTbl())
-ops[bc.TblAdd] = Seq(
-	StoreName('v', Pop()),
-	StoreName('k', Pop()),
-	StoreName('tbl', Pop()),
-	TblSet(LoadName('tbl'), LoadName('k'), LoadName('v')),
-	Pop(),
-	Pop()
-)
-
-ops[bc.CmpEq] = Seq(
-	StoreName('a', Pop()),
-	StoreName('b', Pop()),
-	If(
-		ObjMetalessEq(LoadName('a'), LoadName('b')),
-		Push(TrueAtom),
-		If(
-			And(
-				Eq(Type(LoadName('a')), Int(types.tbl)),
-				Eq(Type(LoadName('b')), Int(types.tbl))
-			),
-			If(
-				Eq(Meta(LoadName('a')), Meta(LoadName('b'))),
-				Push(TrueAtom),-- CALL META
-				Push(FalseAtom)
-			),
-			Push(FalseAtom)
-		)
-	)
-)
+end)
 
 return {
 	mops = mops,
